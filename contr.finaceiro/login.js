79017ca8dc73a0f3b948b086d9c34ef700b79f8e
caio20091999 <caioandrade0920@gmail.com>
2024-05-21 20:53:13 -0300

    login autentic

--- contr.finaceiro/login.js
+++ contr.finaceiro/login.js
@@ -1,3 +1,12 @@
 function logar(){
     var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin" && senha == "admin"){
+        alert('sucesso');
+    }
+
+
+
+
 }
