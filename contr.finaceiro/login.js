c1c81f69e3e35c49eb80ada6f12321b91e021cba
caio20091999 <caioandrade0920@gmail.com>
2024-05-21 21:41:53 -0300

    att css

--- contr.finaceiro/login.js
+++ contr.finaceiro/login.js
@@ -3,7 +3,10 @@ function logar(){
     var senha = document.getElementById('senha').value;
 
     if(login == "admin" && senha == "admin"){
-        alert('sucesso');
+        window.alert('sucesso');
+        location.href="home.html";  
+    }else{
+        window.alert('Usuario ou senha incorretos');
     }
 
 
